2f55b5c3c8c981e8384ad1864a3cddd429550500
mayannkkk19 <hatmario06@gmail.com>
2026-03-30 22:17:03 +0530

    Time: 73 ms (5.17%), Space: 22.4 MB (28.74%) - LeetHub

---
+++ 2840-check-if-strings-can-be-made-equal-with-operations-ii/2840-check-if-strings-can-be-made-equal-with-operations-ii.cpp
@@ -0,0 +1,45 @@
+class Solution {
+public:
+    bool checkStrings(string s1, string s2) {
+        unordered_map<char, int> mp;
+        unordered_map<char, pair<int, int>> mp1;
+        unordered_map<char, pair<int, int>> mp2;
+        int n = s1.size();
+
+        //0 for even, 1 for odd
+
+        for(int i = 0; i < n; i++){
+            if(i%2 == 0){
+                mp1[s1[i]].first++;
+                mp2[s2[i]].first++;
+            }else{
+                mp1[s1[i]].second++;
+                mp2[s2[i]].second++;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            mp[s1[i]]++;
+            mp[s2[i]]--;
+        }
+
+        if(s1 == s2){
+            return true;
+        }
+
+        for(auto x: mp){
+            if(x.second != 0){
+                return false;
+            }
+        }
+
+        for(int i = 0; i < n; i++){
+            if((mp1[s1[i]].first != mp2[s1[i]].first) || (mp1[s1[i]].second != mp2[s1[i]].second) ){
+                return false;
+            }
+        }
+        
+
+        return true;
+    }
+};
